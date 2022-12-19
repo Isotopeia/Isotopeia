34a288a454510ce12fc7b417021bc75acc083243
matthyno <34276633+matthyno@users.noreply.github.com>
2022-12-19 10:17:14 -0500

    Update savemod.js

--- savemod.js
+++ savemod.js
@@ -1,9 +1,9 @@
-var elncn,elnncn,upcn = 0;
+var elncn,elnncn,upcn,prestigeLevel = 0;
 var tosave = [];
 var toload = [[""]];
 var b64 = "";
 function exportSave() {
-	b64 = b64Encode(JSON.stringify([[elncn, elnncn, upcn], [0,0,tosave], "[" + JSON.stringify(toload) + "]"]));
+	b64 = b64Encode(JSON.stringify([[elncn, elnncn, upcn], [0,0,tosave], "[" + JSON.stringify(toload) + "]",prestigeLevel]));
 }
 
 function b64Encode( str ) {
@@ -29,6 +29,7 @@ function updateIds() {
 	document.getElementById("upcns").value = upcn;
 	document.getElementById("tosaves").value = tosave;
 	document.getElementById("toloads").value = toload;
+    document.getElementById("prestiges").value = prestigeLevel;
 	document.getElementById("b64s").value = b64;
 }
 function updateVars() {
@@ -38,4 +39,5 @@ function updateVars() {
 	tosave = document.getElementById("tosaves").value;
 	toload = document.getElementById("toloads").value;
 	b64 = document.getElementById("b64s").value;
+    prestigeLevel = document.getElementById("prestiges").value;
 }
