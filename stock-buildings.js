e05ac06f5695d7d1a5593eec00d5944915325d91
kate <34276633+mechanikate@users.noreply.github.com>
2024-10-09 13:09:18 -0400

    split stockBuidlingsJsonStr off to new file

---
+++ stock-buildings.js
@@ -0,0 +1,118 @@
+const stockBuildingsJsonStr = JSON.stringify([{
+    "buildings": {
+        "en": [{
+            "price": 100,
+            "name": "Bubble chamber",
+            "perSecond": 1,
+            "id": "bubbleChamber"
+        }, {
+            "price": 500,
+            "name": "Particle accelerator",
+            "perSecond": 5,
+            "id": "particleAccelerator"
+        }, {
+            "price": 5000,
+            "name": "Upgraded laboratory",
+            "perSecond": 50,
+            "id": "upgradedLaboratory"
+        }, {
+            "price": 50000,
+            "name": "Fume hood",
+            "perSecond": 5000,
+            "id": "fumeHood"
+        }, {
+            "price": 50000000,
+            "name": "Extraterrestiral research facility",
+            "perSecond": 100000000000,
+            "id": "extraTerraResearchFacility"
+        }, {
+            "price": 500000000000000,
+            "name": "Microcellular automata",
+            "perSecond": 1000000000000000,
+            "id": "microAutomata"
+        }, {
+            "price": 50000000000000000,
+            "name": "Hawking radiaton simulation chamber",
+            "perSecond": 1000000000000000000,
+            "id": "hawkingSimChamber"
+        }, {
+            "price": 500000000000000000000,
+            "name": "Black hole simulation chamber",
+            "perSecond": 100000000000000000000,
+            "id": "blackHoleSimChamber"
+        }],
+        "e": [{
+            "price": 100,
+            "name": "Radioactive beta decay machine",
+            "perSecond": 1,
+            "id": "radioactiveBetaDecayMachine"
+        }, {
+            "price": 1000,
+            "name": "Oudin coil",
+            "perSecond": 10,
+            "id": "oudinCoil"
+        }, {
+            "price": 10000,
+            "name": "Tesla coil",
+            "perSecond": 1000,
+            "id": "teslaCoil"
+        }, {
+            "price": 100000,
+            "name": "Marx generator",
+            "perSecond": 500000,
+            "id": "marxGenerator"
+        }, {
+            "price": 200000000,
+            "name": "Nuclear reactor",
+            "perSecond": 10000000,
+            "id": "nuclearReactor"
+        }, {
+            "price": 30000000000,
+            "name": "Tokamak fusion reactor",
+            "perSecond": 50000000000,
+            "id": "tokamakFusionReactor"
+        }, {
+            "price": 5000000000000,
+            "name": "Gravity extractor",
+            "perSecond": 50000000000000,
+            "id": "gravityExtractor"
+        }, {
+            "price": 800000000000000,
+            "name": "Artificial universe",
+            "perSecond": 50000000000000000,
+            "id": "artificialUniverse"
+        }],
+        "u": [{
+            "price": 10,
+            "name": "Quark generator",
+            "perSecond": 10,
+            "id": "quarkGenerator"
+        }, {
+            "price": 1000,
+            "name": "Matter converter",
+            "perSecond": 1000,
+            "id": "matterConverter"
+        }, {
+            "price": 100000,
+            "name": "Quark simulator",
+            "perSecond": 100000,
+            "id": "quarkSimulator"
+        }, {
+            "price": 1000000,
+            "name": "Quark fusor",
+            "perSecond": 10000000000,
+            "id": "quarkFusor"
+        }, {
+            "price": 200000000000000,
+            "name": "Quark collision chamber",
+            "perSecond": 10000000000000000,
+            "id": "quarkCollider"
+        }, {
+            "price": 500000000000000000000,
+            "name": "Cyclotron",
+            "perSecond": 1000000000000000000,
+            "id": "cyclotron"
+        }]
+    },
+    "customBehaviors": ""
+}]);
