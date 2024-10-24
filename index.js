946484656b8c0f767e16780398292519cbeee0d8
kate <34276633+mechanikate@users.noreply.github.com>
2024-10-24 11:35:43 -0400

    add conditional

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ function addItems() { // load the jtopia upgrades
     update();
 }
 var actuallySave = true; // flag to actually save it
-const confirmReset = () => if(true) resetNoconfirm(); // TODO: add conditional checking if box is checked
+const confirmReset = () => if(document.getElementById("resetcheckbox").checked) resetNoconfirm();
 function resetNoconfirm() { // reset but don't ask for confirmation!
 	actuallySave = false;
 	localStorage.setItem("ucc", JSON.stringify([0, 0, 0]));
