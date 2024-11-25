8b7e34f4d2dc9ceb3ab5d6cf881986b166c99a19
kate <34276633+mechanikate@users.noreply.github.com>
2024-11-25 11:27:48 -0500

    e+0 no longer displays fractions of units

--- index.js
+++ index.js
@@ -263,4 +263,4 @@ function updatePerSecond() {
 	uppc = 1+Math.floor(upps / 15);
 }
 
-const toUnitName = amt => `${(amt/Math.pow(10,Math.floor(Math.log10(amt)/3)*3)).toFixed(3)} ${unitList[Math.floor(Math.log10(amt)/3)]}`;
+const toUnitName = amt => `${(amt/Math.pow(10,Math.floor(Math.log10(Math.floor(amt))/3)*3)).toFixed(3)} ${unitList[Math.floor(Math.log10(Math.floor(amt))/3)]}`;
